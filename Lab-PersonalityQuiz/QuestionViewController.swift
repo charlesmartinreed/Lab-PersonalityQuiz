83e7026b7503fe505721b6d972e7a01b8b5a296f
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-11-01 18:25:04 -0500

    Add logic to register and store user's chosen answers.

--- Lab-PersonalityQuiz/QuestionViewController.swift
+++ Lab-PersonalityQuiz/QuestionViewController.swift
@@ -12,6 +12,7 @@ class QuestionViewController: UIViewController {
 
     //MARK:- Properties
     var questionIndex = 0 // keep track of how many questions have been answered
+    var answersChosen: [Answer] = []
     
     //MARK:- @IBOutlets
     @IBOutlet weak var questionLabel: UILabel!
@@ -27,19 +28,79 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var multiLabel2: UILabel!
     @IBOutlet weak var multiLabel3: UILabel!
     @IBOutlet weak var multiLabel4: UILabel!
-    
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
     
     @IBOutlet weak var rangedStackView: UIStackView!
     @IBOutlet weak var rangedLabel1: UILabel!
     @IBOutlet weak var rangedLabel2: UILabel!
+    @IBOutlet weak var rangedSlider: UISlider!
     
     @IBOutlet weak var questionProgressView: UIProgressView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
         
+    }
+    
+    //MARK:- @IBActions
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        // when the user clicks a button, they've chosen an answer.
+        // Pull it from the questions array and then append it to the answersChosen array.
+        switch sender {
+        case singleButton1:
+            answersChosen.append(currentAnswers[0])
+        case singleButton2:
+            answersChosen.append(currentAnswers[1])
+        case singleButton3:
+            answersChosen.append(currentAnswers[2])
+        case singleButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            break
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        //slider value is 0-1, value 0-.25 is first answer, value 0.75-1 is last answer
+        let currentAnswers = questions[questionIndex].answers
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        answersChosen.append(currentAnswers[index])
+        
+        nextQuestion()
+        
+    }
+    
+    
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        // check which switches are enabled, use that to determine what answer to add to the answersChosen array
+        let currentAnswers = questions[questionIndex].answers
+        
+        if multiSwitch1.isOn {
+            answersChosen.append(currentAnswers[0])
+        }
+        
+        if multiSwitch2.isOn {
+            answersChosen.append(currentAnswers[1])
+        }
+        
+        if multiSwitch3.isOn {
+            answersChosen.append(currentAnswers[2])
+        }
+        
+        if multiSwitch4.isOn {
+            answersChosen.append(currentAnswers[3])
+        }
+        
+        nextQuestion()
     }
     
     
@@ -96,6 +157,10 @@ class QuestionViewController: UIViewController {
         //because there are only two labels to update but a variable amount of answers, we use first and last to access collection indices
         rangedLabel1.text = answers.first?.text
         rangedLabel2.text = answers.last?.text
+    }
+    
+    func nextQuestion() {
+        
     }
 
 }
